735cb78fb888f0943580bea4fa87d79a61d53e85
jovezen <jovezen@gmail.com>
2021-04-16 19:31:33 +0000

    Polished struct

--- Lotus-testnet/SetupNode.sh
+++ Lotus-testnet/SetupNode.sh
@@ -6,7 +6,7 @@ echo ✰ Download daemon
 wget https://raw.githubusercontent.com/zenchainprotocol/Launchpad/main/build/v1.1.4/$DAEMON_NAME.zip
 unzip $DAEMON_NAME.zip
 chmod 777 $DAEMON_NAME/zenchaind
-cp -f $DAEMON_NAME/zenchaind /usr/local/bin/zenchaind
+cp -f $DAEMON_NAME/zenchaind ~/zenchaind
 
 echo ╔═════════  ZENCHAIN-Lotus-testnet ═════════╗
 echo ║                                           ║
@@ -17,15 +17,15 @@ echo ✰ What your moniker name?
 read MONIKER
 
 cd ..
-zenchaind init $MONIKER --chain-id=lotus-tesnet
+~/zenchaind init $MONIKER --chain-id=lotus-tesnet
 
 echo ✰ Download genesis and add persistent peers
-wget https://raw.githubusercontent.com/zenchainprotocol/Launchpad/main/Lotus-testnet/genesis.json
-cp -f ./genesis.json ~/.zenchain/config/genesis.json 
+wget https://raw.githubusercontent.com/zenchainprotocol/Launchpad/main/Lotus-testnet/genesis-one.json
+cp -f ./genesis-one.json ~/.zenchain/config/genesis.json 
 sed -i 's/persistent_peers = ""/persistent_peers = "d087db1d451a9e1b52e5e20848c416e6be8d7f0a@45.63.22.19:26656"/g' ~/.zenchain/config/config.toml
 
 echo ✰ Start your node
-zenchaind start
+~/zenchaind start
 
 rm -r zentemp
 echo ✰✰✰✰ DONE ✰✰✰✰✰
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Launchpad
-Responsive for run zenchain (testnet, beta net, main net) blockchain
+Repo contain things for run zenchain (testnet, beta net, main net) blockchain:
+
+    - Bash script
+    - Genesis file
+    - Node daemon for mutil OS
+
+## Folder struct
+### /build
+Contains all daemon with versions tag
+### /...
+Contains all Bash script and Genesis file for each network. Example: /Lotus-testnet
