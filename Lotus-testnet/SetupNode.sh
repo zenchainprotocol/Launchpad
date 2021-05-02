cb53497844cdb7b772bac653690f80c226bbded2
Zenchaindev <zenchain102@gmail.com>
2021-05-02 10:46:14 +0700

    Reset Lotus-testnet

--- Lotus-testnet/SetupNode.sh
+++ Lotus-testnet/SetupNode.sh
@@ -22,7 +22,7 @@ cd ..
 echo ✰ Download genesis and add persistent peers
 wget https://raw.githubusercontent.com/zenchainprotocol/Launchpad/main/Lotus-testnet/genesis.json
 cp -f ./genesis.json ~/.zenchain/config/genesis.json 
-sed -i 's/persistent_peers = ""/persistent_peers = "be83b75c88a0ba30bfed072fd805c10ab3dac8ac@45.77.251.139:26656"/g' ~/.zenchain/config/config.toml
+sed -i 's/persistent_peers = ""/persistent_peers = "d087db1d451a9e1b52e5e20848c416e6be8d7f0a@45.63.22.19:26656"/g' ~/.zenchain/config/config.toml
 
 echo ✰ Start your node
 ~/zenchaind start
